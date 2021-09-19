0eb265d8c15a1a58ef708b059a4955e29778015b
Mehedi Hasan Sarkar <mehedisarkar@hotmail.com>
2021-09-19 21:00:13 +0600

    added increase decrease button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
+import { useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
+
+const employeeArr = [
+  { id: "1", title: "", name: "Mehedi Sarkar", salary: "50tk/day" },
+  { id: "2", title: "Mr.", name: "Bean", salary: "300k" },
+  { id: "3", title: "Mrs.", name: "Emma Watson", salary: "200k" },
+];
+
+const Employees = ({ title, name, salary }) => {
+  return (
+    <div className="Employee">
+      <h2>
+        Name: {title} {name}
+      </h2>
+      <span>Salary: {salary}</span>
+    </div>
+  );
+};
+
+const ControlBTN = () => {
+  let [count, setCount] = useState(0);
+  const increase = () => {
+    if (count >= 20) return;
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    if (count <= 0) return;
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <span className="value">Count: {count}</span>
+      <div className="buttons">
+        <button onClick={decrease}>Decrease(-)</button>
+        <button onClick={increase}>Increase(+)</button>
+      </div>
+    </div>
+  );
+};
+
+const LoadCountries = () => {
+  return null;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Employees">
+        {employeeArr.map((employee) => (
+          <Employees
+            key={employee.id + "-" + employee.name}
+            title={employee.title}
+            name={employee.name}
+            salary={employee.salary}
+          />
+        ))}
+      </div>
+
+      <div className="controlButton">
+        <ControlBTN />
+      </div>
+
+      <div className="countries">
+        <LoadCountries />
+      </div>
     </div>
   );
 }
